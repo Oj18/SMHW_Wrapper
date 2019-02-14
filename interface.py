d0f7c38cdd3328991021fcdccd2b2b48c319cf90
Jeff <ojmed1803@gmail.com>
2019-02-14 20:32:14 +0000

    v0.2.0 Release

--- interface.py
+++ interface.py
@@ -1,22 +1,13 @@
-# using requests for HTTP and json for, um... JSON
 import requests
 import json
-import asyncio
-
-debug = True  # DO NOT USE unless you know what you're doing! This overrides errors!
+import time
 
 
-# a quick note - random tests led me to discover a school with subdomain "lololol", id 251616 - use for testing
-
-# let's start with custom exceptions
-
-class NotStoredError(Exception):
+class HomeworkNotFound(Exception):
     pass
-
 
 class CredentialError(Exception):
     pass
-
 
 class NoAuthAPI:
     def public_api_request(location):
@@ -27,18 +18,28 @@ class NoAuthAPI:
         return requests.get("https://api.showmyhomework.co.uk/api/" + location,
                             headers={"Accept": "application/smhw.v3+json"}).text  # HTTPS request to the server
 
+    def get_homework(id):
+        request = NoAuthAPI.regular_api_request('homeworks/' + str(id))
+        
+        if request == '':
+            raise HomeworkNotFound("Homework Not Found")
+        
+        return Homework(json.loads(request)['homework'])
+
+class Homework:
+    def __init__(self, properties):
+        self.properties = properties
+        for key in self.properties:
+            setattr(self, key, self.properties[key])
+
+
 # main class - all operations are called from in here
 class School:
     def __init__(self, sub, autodownload=False, count=0):
         self.subdomain = sub
         self.id = self.get_school_id(sub, count)
 
-        self.links = self.make_links();
-        self.count = count
-        self.employess = self.get_employees()
-
-    def make_links():
-        return {
+        self.links = {
             'students': 'students?school_id=' + str(self.id),
             'parents': 'parents?school_id=' + str(self.id),
             'employees': 'employees?school_id=' + str(self.id),
@@ -48,7 +49,22 @@ class School:
             'class_groups': 'class_groups?school_id=' + str(self.id),
             'kudos_reasons': 'kudos_reasons?school_id=' + str(self.id)
         }
-    
+
+        self.count = count
+        self.employess = self.get_employees()
+
+    """def make_links():
+        return {
+            'students': 'students?school_id=' + str(self.id),
+            'parents': 'parents?school_id=' + str(self.id),
+            'employees': 'employees?school_id=' + str(self.id),
+            'class_years': 'class_years?school_id=' + str(self.id),
+            'subjects': 'subjects?school_id=' + str(self.id),
+            'house_groups': 'house_groups?school_id=' + str(self.id),
+            'class_groups': 'class_groups?school_id=' + str(self.id),
+            'kudos_reasons': 'kudos_reasons?school_id=' + str(self.id)
+        }"""
+
     def get_school_id(self, subdomain, count=0):
         try:
             data = json.loads(NoAuthAPI.public_api_request("school_search?subdomain=" + subdomain))["schools"][count]["id"]
@@ -69,8 +85,7 @@ class User:
         self.school = school
         self.username = username
         self.password = password
-        self.loop = asyncio.get_event_loop()
-
+        
         payload = {"client_id": "55283c8c45d97ffd88eb9f87e13f390675c75d22b4f2085f43b0d7355c1f",
                     "client_secret": "c8f7d8fcd0746adc50278bc89ed6f004402acbbf4335d3cb12d6ac6497d3"}
 
@@ -78,20 +93,25 @@ class User:
                 "grant_type": "password", "school_id": self.school.id}
         temp = requests.post("https://api.showmyhomework.co.uk/oauth/token", params=payload, data=data,
                             headers={"Accept": "application/smhw.v3+json"})
-        try:
-            self.token = temp.headers["smhw_token"]
-            self.token_expires = temp.headers["expires_in"]
-            self.refresh_token = temp.headers["refresh_token"]
-        except KeyError:
-            global debug
-            if not debug:
-                raise CredentialError
-            else:
-                print("Credentials invalid, please inject!")
 
-        self.loop.run_until_complete(self.wait_for_oauth_token_refresh())
+        try:
+            self.token = temp.json()["smhw_token"]
+            self.token_expires_in = temp.json()["expires_in"]
+            self.refresh_token = temp.json()["refresh_token"]
+            self.token_created_at = temp.json()["created_at"]
+            self.token_expires_at = self.token_created_at + self.token_expires_in
+        except KeyError:
+            raise CredentialError("Invalid Credentials")
+        
+        #self.id = self.get_user_id()
+
+    def private_infos(include='user_private_info,school'): #useful info
+        return self.user_api_request('students/' + self.id + '?include=' + include)
 
     def user_api_request(self, location, auth=True):
+        if (int(time.time()) >= self.token_expires_at):
+            self.refresh_oauth_token()
+        
         if auth:
             headers = {"Accept": "application/smhw.v3+json", "Authorization": "Bearer " + self.token}
         else:
@@ -105,8 +125,9 @@ class User:
         data = {"grant_type": "refresh_token", "refresh_token": self.refresh_token, "school_id": self.school.id}
         temp = requests.post("https://api.showmyhomework.co.uk/oauth/token", params=payload, data=data,
                             headers={"Accept": "application/smhw.v3+json"})
-        self.loop.run_until_complete(self.wait_for_oauth_token_refresh())
 
-    async def wait_for_oauth_token_refresh(self):
-        await asyncio.sleep(self.token_expires)
-        self.refresh_token()
+        self.token = temp.json()["smhw_token"]
+        self.token_expires_in = temp.json()["expires_in"]
+        self.refresh_token = temp.json()["refresh_token"]
+        self.token_created_at = temp.json()["created_at"]
+        self.token_expires_at = self.token_created_at + self.token_expires_in
--- readme.md
+++ readme.md
@@ -1,17 +1,38 @@
 # SMHW_Wrapper
+
 An API Wrapper for Show My Homework made in Python. Based on [@SamPoulton's Wrapper](https://github.com/SamPoulton/smhw-python), but with major fixes and changes.
 
 ---
 
 ## Changelog
 
+---
+
+### v0.2.0
+
+- Added `Homework` class
+
+- Added `HomeworkNotFound` exception
+
+- Added `NoAuthAPI.get_homework(id)` - Returns a `Homework` when given a `Homework id`
+
+- Removed unused `NotStoredError`
+
+- Removed `debug` stuff
+
+- Actually fixed token refreshing
+
+- Fixed `User` class (logging in)
+
+---
+
 ### v0.1.0
 
 - Fixed Token Refreshing
 
-- Major overhaul of School class
+- Major overhaul of `School` class
   - Fixed link getting
   - Removed API requests
 
-- Added 'NoAuthAPI' class
-  - Replaces API requests in School class
+- Added `NoAuthAPI` class
+  - Replaces API requests in `School`
